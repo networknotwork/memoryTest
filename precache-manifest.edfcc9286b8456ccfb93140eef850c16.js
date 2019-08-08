a2469e2cd95b04384fb28cbe5753f2f63260f0ae
Rusty Boot <nicholastuso@yahoo.com>
2019-08-08 14:15:47 -0700

    Updates

---
+++ precache-manifest.edfcc9286b8456ccfb93140eef850c16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901e8689c6901040d7fb7e45af9e817f",
+    "url": "/memoryTest/index.html"
+  },
+  {
+    "revision": "493500ce104a5112ebcb",
+    "url": "/memoryTest/static/css/main.4a033a60.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memoryTest/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "493500ce104a5112ebcb",
+    "url": "/memoryTest/static/js/main.a942f492.chunk.js"
+  },
+  {
+    "revision": "1a9cf4a6431ee38de7ac",
+    "url": "/memoryTest/static/js/runtime~main.038a7a65.js"
+  }
+]);
